96ccbc802f351d4dc14630d04f5561be74b3450b
nathanseixas <nathandiniz19042004@gmail.com>
2024-12-14 16:04:03 -0300

    criando a tablea e mapeando os fornecedores

--- src/pages/Fornecedor/FornecedorList.jsx
+++ src/pages/Fornecedor/FornecedorList.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React from 'react'
+import { useState, useEffect } from 'react'
 import axios from '../../api'
-import { FaPlus } from 'react-icons/fa'
+import { FaEdit, FaPlus, FaTrash } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 const FornecedorList = () => {
@@ -11,9 +11,7 @@ const FornecedorList = () => {
     useEffect(() => {
         axios.get("/fornecedores")
         .then(response => setFornecedores(response.data))
-        .catch(error => console.log("Error ao carregar fornecedores: ", error))
-
-
+        .catch(error => console.error("Error ao carregar fornecedores: ", error))
     }, []) 
 
 
@@ -23,6 +21,36 @@ const FornecedorList = () => {
 
         <Link to="/add-fornecedores" className="btn btn-primary mb-2">
         <FaPlus className="icon"/> Adicionar Fornecedor</Link>
+
+        <table className="table">
+            <thead>
+                <th>Nome</th>
+                <th>CNPJ</th>
+                <th>Email</th>
+                <th>Ações</th>
+            </thead>
+            <tbody>
+                {
+                    fornecedores.map(fornecedor => (
+                        <tr key={fornecedor.id}>
+                            <td>{fornecedor.nome}</td>
+                            <td>{fornecedor.cnpj}</td>
+                            <td>{fornecedor.email}</td>
+                            <td>
+                                <Link to={`/edit-fornecedores/${fornecedor.id}`} className="btn btn-sm btn-warning">
+                                <FaEdit className="icon icon-btn" />Editar</Link>
+                                <button className="btn btn-sm btn-danger">
+                                    <FaTrash className="icon icon-btn" />
+                                    Excluir
+
+                                </button>
+                            </td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+
+        </table>
 
     </div>
   )
