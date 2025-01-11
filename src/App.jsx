2ebf1227a723c0f2315ad4e45534956f33790245
nathanseixas <nathandiniz19042004@gmail.com>
2025-01-11 09:59:26 -0300

    atualizando o código

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
 import FornecedorList from './pages/Fornecedor/FornecedorList'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Inicial from './pages/Inicial'
+import FornecedorForm from './pages/Fornecedor/FornecedorForm'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <FornecedorList />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Inicial />} />
+          <Route path="/listar-fornecedores" element={<FornecedorList />} />
+          <Route path="/add-fornecedores" element={<FornecedorForm />} />
+          <Route path="/edit-fornecedores/:id" element={<FornecedorForm />} />
+        </Routes>
+      </BrowserRouter>
+    
   )
 }
 
--- src/pages/Fornecedor/FornecedorList.jsx
+++ src/pages/Fornecedor/FornecedorList.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from '../../api'
-import { FaCheckCircle, FaEdit, FaExclamationTriangle, FaPlus, FaTrash } from 'react-icons/fa'
+import { FaCheckCircle, FaEdit, FaExclamation, FaExclamationTriangle, FaPlus, FaQuestionCircle, FaTrash } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import Modal from 'react-modal'
 
@@ -10,19 +9,18 @@ const FornecedorList = () => {
     const [fornecedores, setFornecedores] = useState([])
     const [fornecedorSelecionado, setFornecedorSelecionado] = useState(null)
     const [modalAberto, setModalAberto] = useState(false)
-    const [modalSucessoAbertot, setModalSucessoAberto ] = useState(false)
+    const [modalSucessoAberto, setModalSucessoAberto] = useState(false)
+    const [tooltipAberto, setTooltipAberto] = useState(false)
 
     useEffect(() => {
         axios.get("/fornecedores")
         .then(response => setFornecedores(response.data))
-        .catch(error => console.error("Error ao carregar fornecedores: ", error))
-    }, []) 
+        .catch(error => console.error("Erro ao carregar fornecedores: ", error))
+    }, [])
 
-    
-    const abirModal = (fornecedor) => {
+    const abrirModal = (fornecedor) => {
         setFornecedorSelecionado(fornecedor)
         setModalAberto(true)
-
     }
 
     const fecharModal = () => {
@@ -38,42 +36,49 @@ const FornecedorList = () => {
     const removerFornecedor = () => {
         axios.delete(`/fornecedores/${fornecedorSelecionado.id}`)
         .then(() => {
-            setFornecedores(prevFornecedores => prevFornecedores.filter 
-                (fornecedor => fornecedor.id != fornecedorSelecionado.id))
-                fecharModal()
-                abirModal()
+            setFornecedores(prevFornecedores => prevFornecedores.filter(fornecedor => fornecedor.id !== fornecedorSelecionado.id))
+            fecharModal()
+            abrirModalSucesso()
         })
     }
 
+    const toggleTooltip = () => {
+        setTooltipAberto(!tooltipAberto)
+    }
 
   return (
     <div className="container mt-5">
-        <h2 className="mb-4" style={{ position: 'relative' }}>Lista de Fornecedores</h2>
+        <h2 className="mb-4" style={{ position: 'relative' }}>Lista de Fornecedores
+            <FaQuestionCircle
+                className="tooltip-icon"
+                onClick={toggleTooltip}
+            />
+            {tooltipAberto && (<div className="tooltip">
+                Aqui você pode ver, editar ou excluir fornecedores cadastrados no sistema.
+            </div>)}
 
-        <Link to="/add-fornecedores" className="btn btn-primary mb-2">
-        <FaPlus className="icon"/> Adicionar Fornecedor</Link>
+        </h2>
+
+        <Link to="/add-fornecedores" className="btn btn-primary mb-2"> <FaPlus className="icon" /> Adicionar Fornecedor</Link>
 
         <table className="table">
             <thead>
-                <th>Nome</th>
-                <th>CNPJ</th>
-                <th>Email</th>
-                <th>Ações</th>
+                <th>Nome:</th>
+                <th>CNPJ:</th>
+                <th>Email:</th>
+                <th>Ações:</th>
             </thead>
             <tbody>
                 {
                     fornecedores.map(fornecedor => (
                         <tr key={fornecedor.id}>
                             <td>{fornecedor.nome}</td>
-                            <td>{fornecedor.cnpj}</td>
+                            <td>{fornecedor.cnpj.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, "$1.$2.$3/$4-$5")}</td>
                             <td>{fornecedor.email}</td>
                             <td>
-                                <Link to={`/edit-fornecedores/${fornecedor.id}`} className="btn btn-sm btn-warning">
-                                <FaEdit className="icon icon-btn" />Editar</Link>
-                                <button onClick={()=> abirModal(fornecedor)} className="btn btn-sm btn-danger">
-                                    <FaTrash className="icon icon-btn" />
-                                    Excluir
-
+                                <Link to={`/edit-fornecedores/${fornecedor.id}`} className="btn btn-sm btn-warning"><FaEdit className="icon icon-btn" /> Editar</Link>
+                                <button onClick={() => abrirModal(fornecedor)} className="btn btn-sm btn-danger">
+                                   <FaTrash className="icon icon-btn" /> Excluir
                                 </button>
                             </td>
                         </tr>
@@ -85,36 +90,34 @@ const FornecedorList = () => {
 
         <Modal
             isOpen={modalAberto}
+            onRequestClose={fecharModal}
             className="modal"
             overlayClassName="overlay"
         >
             <div className="modalContent">
                 <FaExclamationTriangle className="icon" />
                 <h2>Confirmar Exclusão</h2>
-                <p>Tem certeza que deseja excluir o fornecedor
-                {fornecedorSelecionado && fornecedorSelecionado.nome}?</p>
+                <p>Tem certeza que deseja excluir o fornecedor <br/>{fornecedorSelecionado && fornecedorSelecionado.nome}?
+                </p>
                 <div className="modalButtons">
                     <button onClick={fecharModal} className="btn btn-secondary">Cancelar</button>
                     <button onClick={removerFornecedor} className="btn btn-danger">Excluir</button>
-
                 </div>
 
-
             </div>
-            
+
         </Modal>
 
 
-        <Modal 
-            isOpen={modalSucessoAbertot}
+        <Modal
+            isOpen={modalSucessoAberto}
             onRequestClose={() => setModalSucessoAberto(false)}
             className="modal"
             overlayClassName="overlay"
-
         >
-            <div className="ModalCoctent">
-                <FaCheckCircle className="icon sucessIcon" />
-                <h2>Fornecedor excluido com sucesso!</h2>
+            <div className="modalContent">
+                <FaCheckCircle className="icon successIcon" />
+                <h2>Fornecedor excluído com sucesso!</h2>
             </div>
 
         </Modal>
