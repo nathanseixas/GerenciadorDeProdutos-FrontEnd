763e909f02ec96d02e34f2ce00f4668d4a4bc9a9
nathanseixas <nathandiniz19042004@gmail.com>
2025-01-11 11:29:42 -0300

    Subindo ProdutosList

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Inicial from './pages/Inicial'
 import FornecedorForm from './pages/Fornecedor/FornecedorForm'
 import Navbar from './components/Navbar'
+import ProdutoForm from './pages/Produto/ProdutoForm'
+import ProdutoList from './pages/Produto/ProdutoList'
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
           <Route path="/listar-fornecedores" element={<FornecedorList />} />
           <Route path="/add-fornecedores" element={<FornecedorForm />} />
           <Route path="/edit-fornecedores/:id" element={<FornecedorForm />} />
+          <Route path="/add-produtos" element={<ProdutoForm  />} />
+          <Route path="/listar-produtos" element={<ProdutoList  />} />
         </Routes>
       </BrowserRouter>
     
