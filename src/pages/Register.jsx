1484995c47df95f077f7c1ee4317d1dadd111fce
Nathan <nathandiniz19042004@gmail.com>
2025-04-05 12:54:13 -0300

    index.js, Navbar.js, authCOntext, REgister, atualizados.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -21,7 +21,7 @@ const Register = () => {
     e.preventDefault()
     setErro(null)
     try {
-      const response = await fetch('https://localhost:8080', {
+      const response = await fetch('http://localhost:8080', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
